a9e5b3d1bb3d4da007414f1091e926a0feff4807
Oliver Hamlet <git@ortham.net>
2024-08-24 11:12:59 +0100

    Update Google Test to 1.15.2

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -15,8 +15,8 @@ set(BUILD_SHARED_LIBS_INITIAL ${BUILD_SHARED_LIBS})
 set(BUILD_SHARED_LIBS OFF)
 FetchContent_Declare(
     GTest
-    URL "https://github.com/google/googletest/archive/refs/tags/v1.14.0.tar.gz"
-    URL_HASH "SHA256=8ad598c73ad796e0d8280b082cebd82a630d73e73cd3c70057938a6501bba5d7"
+    URL "https://github.com/google/googletest/archive/refs/tags/v1.15.2.tar.gz"
+    URL_HASH "SHA256=7b42b4d6ed48810c5362c265a17faebe90dc2373c885e5216439d37927f02926"
     FIND_PACKAGE_ARGS)
 
 FetchContent_Declare(
