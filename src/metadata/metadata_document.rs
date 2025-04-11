a249b66f422a1e7340a1d5ba1801478b4137096f
Oliver Hamlet <git@ortham.net>
2025-04-11 21:45:36 +0100

    Remove unnecessary semicolon

--- src/metadata/metadata_document.rs
+++ src/metadata/metadata_document.rs
@@ -131,7 +131,7 @@ impl MetadataDocument {
                         YamlObjectType::PluginMetadata,
                     )
                     .into());
-                };
+                }
             }
         }
 
