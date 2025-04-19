99412b02e4c1f045f979418884b8bd0099944388
Oliver Hamlet <git@ortham.net>
2025-04-19 18:27:51 +0100

    Update changelogs and version for 0.26.0

--- include/loot/loot_version.h
+++ include/loot/loot_version.h
@@ -34,10 +34,10 @@ namespace loot {
 inline constexpr unsigned int LIBLOOT_VERSION_MAJOR = 0;
 
 /** @brief libloot's minor version number. */
-inline constexpr unsigned int LIBLOOT_VERSION_MINOR = 25;
+inline constexpr unsigned int LIBLOOT_VERSION_MINOR = 26;
 
 /** @brief libloot's patch version number. */
-inline constexpr unsigned int LIBLOOT_VERSION_PATCH = 5;
+inline constexpr unsigned int LIBLOOT_VERSION_PATCH = 0;
 
 /**
  * @brief Get the library version.
