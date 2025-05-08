0947b23f2b40ca1a58026ec5420bd33965c1a275
Oliver Hamlet <git@ortham.net>
2025-05-08 16:02:36 +0100

    Fix missing CTest discovery of internals tests

--- cpp/cmake/tests.cmake
+++ cpp/cmake/tests.cmake
@@ -106,6 +106,7 @@ add_dependencies(libloot_tests loot)
 target_link_libraries(libloot_tests PRIVATE loot GTest::gtest_main)
 
 enable_testing()
+gtest_discover_tests(libloot_internals_tests DISCOVERY_TIMEOUT 10)
 gtest_discover_tests(libloot_tests DISCOVERY_TIMEOUT 10)
 
 ##############################
