loot/libloot-rs

Switch to the regress crate for regex

src/metadata/plugin_metadata.rs
@@ -1,9 +1,9 @@
 use std::borrow::Cow;
 
-use fancy_regex::{Error as RegexImplError, Regex};
+use regress::{Error as RegexImplError, Regex};
 use saphyr::MarkedYaml;
 
-use crate::{case_insensitive_regex, logging};
+use crate::case_insensitive_regex;
 
 use super::{
     error::{MetadataParsingErrorReason, ParseMetadataError, RegexError},
@@ -241,9 +241,7 @@ impl PluginName {
 
     fn matches(&self, other_name: &str) -> bool {
         if let Some(regex) = &self.regex {
-            regex.is_match(other_name).inspect_err(|e| {
-                logging::error!("Encountered an error while trying to match the regex {} to the string {}: {}", regex.as_str(), other_name, e);
-            }).unwrap_or(false)
+            regex.find(other_name).is_some()
         } else {
             unicase::eq(self.string.as_str(), other_name)
         }

loot/libloot

src/metadata/plugin_metadata.rs
@@ -1,9 +1,9 @@
 use std::borrow::Cow;
 
-use fancy_regex::{Error as RegexImplError, Regex};
+use regress::{Error as RegexImplError, Regex};
 use saphyr::MarkedYaml;
 
-use crate::{Database, case_insensitive_regex, error::ConditionEvaluationError, logging};
+use crate::{Database, case_insensitive_regex, error::ConditionEvaluationError};
 
 use super::{
     error::{MetadataParsingErrorReason, ParseMetadataError, RegexError},
@@ -381,17 +381,7 @@ fn replace_capturing_groups(regex_string: &str) -> Cow<'_, str> {
 }
 
 fn is_regex_match(regex: &Regex, string: &str) -> bool {
-    regex
-        .is_match(string)
-        .inspect_err(|e| {
-            logging::error!(
-                "Encountered an error while trying to match the regex {} to the string {}: {}",
-                regex.as_str(),
-                string,
-                e
-            );
-        })
-        .unwrap_or(false)
+    regex.find(string).is_some()
 }
 
 impl TryFromYaml for PluginMetadata {